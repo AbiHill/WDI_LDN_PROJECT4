4949bf2b2fde7980a133302e3d221c290a411c44
AbiHill <abihill8@gmail.com>
2018-04-05 14:55:08 +0100

    added bablerc etc

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// hello
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.static(`${__dirname}/public`));
--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   render() {
     return (
-      <h1>WDI32 Project 4</h1>
+      <section>
+        <h1>WDI32 Project 4</h1>
+      </section>
     );
   }
 }
